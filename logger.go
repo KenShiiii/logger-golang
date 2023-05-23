f19d22a3230c046f53b6874c4365f378a3c375d5
Kenshi Kuo <kenshi_kuo@hitrontech.com>
2023-05-23 17:52:01 +0800

    implement logger options & logger constructor

--- logger.go
+++ logger.go
@@ -9,79 +9,87 @@ import (
 	"time"
 )
 
-// Logger represents a logger instance with a specific log level.
-type Logger struct {
-	// LogLevel specifies the minimum log level required for a log message to be printed.
-	LogLevel int
+// Logger is the interface difinition for a simple logger as used by this library.
+type Logger interface {
+	Close()
+	Log(level int, args ...interface{})
+}
 
-	// FilePath specifies the path to the log file.
-	FilePath string
+type logger struct {
+	logLevel int // LogLevel specifies the minimum log level required for a log message to be printed.
 
-	// file is the file handle for the log file.
-	File *os.File
+	filePath string // FilePath specifies the path to the log file.
+
+	file *os.File // file is the file handle for the log file.
 }
 
 // Close closes the log file.
-func (l *Logger) Close() {
-	if l.File != nil {
-		l.File.Close()
-		l.File = nil
+func (l *logger) Close() {
+	if l.file != nil {
+		l.file.Close()
+		l.file = nil
 	}
 }
 
-func NewLogger() *Logger {
-	return &Logger{}
+func NewLogger(o *loggerOptions) Logger {
+	l := &logger{
+		logLevel: o.logLevel,
+		file:     o.logFile,
+	}
+
+	return l
 }
 
 // Log prints a log message with the given log level and message arguments.
 // If the log level is less than the logger's LogLevel, the message is not printed.
-func (l *Logger) Log(level int, args ...interface{}) {
-	if level >= l.LogLevel {
-		var prefix string
-		switch level {
-		case 0:
-			// Debug level
-			prefix = "\033[38;5;16;48;5;253mDEBU\033[0m "
-		case 1:
-			// Info level
-			prefix = "\033[48;5;57mINFO\033[0m "
-		case 2:
-			// Warning level
-			prefix = "\033[38;5;16;48;5;226mWARN\033[0m "
-		case 3:
-			// Error level
-			prefix = "\033[38;5;15;48;5;160mERRO\033[0m "
-		default:
-			// Unknown level
-			prefix = "\033[38;5;15;48;5;82mUNKNOWN LEVEL\033[0m "
-		}
-
-		// empty line to seperate log
-		// fmt.Println()
-		_, file, line, ok := runtime.Caller(1)
-		if !ok {
-
-			file = ""
-			line = 0
-		}
-
-		fileName := path.Base(file)
-		// shottFuncName := path.Base(runtime.FuncForPC(funcName).Name())
-
-		fmt.Printf("[%s] %s[%s:%d] %s \n", time.Now().Local().Format("2006-01-02 15:04:05.000 UTC-0700"), prefix, fileName, line, fmt.Sprint(args...))
-
-		// log.Printf("%s%s", prefix, fmt.Sprint(args...))
-
-		// Replace escape characters with an empty string
-		re := regexp.MustCompile(`\033\[[0-9;]*[a-zA-Z]`)
-		prefix = re.ReplaceAllString(prefix, "")
-		argsStr := re.ReplaceAllString(fmt.Sprint(args...), "")
-
-		logTime := time.Now().Local().Format("2006-01-02 15:04:05.000 UTC-0700")
-
-		// write to log file
-		if l.File != nil {
-			fmt.Fprintf(l.File, "\n%s %s%s", logTime, prefix, argsStr)
-		}
+func (l *logger) Log(level int, args ...interface{}) {
+	if level < l.logLevel {
+		return
 	}
+	var prefix string
+	switch level {
+	case 0:
+		// Debug level
+		prefix = "\033[38;5;16;48;5;253mDEBU\033[0m "
+	case 1:
+		// Info level
+		prefix = "\033[48;5;57mINFO\033[0m "
+	case 2:
+		// Warning level
+		prefix = "\033[38;5;16;48;5;226mWARN\033[0m "
+	case 3:
+		// Error level
+		prefix = "\033[38;5;15;48;5;160mERRO\033[0m "
+	default:
+		// Unknown level
+		prefix = "\033[38;5;15;48;5;82mUNKNOWN LEVEL\033[0m "
+	}
+
+	// empty line to seperate log
+	// fmt.Println()
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+
+		file = ""
+		line = 0
+	}
+
+	fileName := path.Base(file)
+	// shottFuncName := path.Base(runtime.FuncForPC(funcName).Name())
+	logTime := time.Now().Local().Format("2006-01-02 15:04:05.000 UTC-0700")
+
+	fmt.Printf("[%s] %s[%s:%d] %s \n", logTime, prefix, fileName, line, fmt.Sprint(args...))
+
+	// log.Printf("%s%s", prefix, fmt.Sprint(args...))
+
+	// Replace escape characters with an empty string
+	re := regexp.MustCompile(`\033\[[0-9;]*[a-zA-Z]`)
+	prefix = re.ReplaceAllString(prefix, "")
+	argsStr := re.ReplaceAllString(fmt.Sprint(args...), "")
+
+	// write to log file
+	if l.file != nil {
+		fmt.Fprintf(l.file, "[%s] %s[%s:%d] %s \n", logTime, prefix, fileName, line, argsStr)
+	}
+
 }
--- options.go
+++ options.go
@@ -4,5 +4,24 @@ import "os"
 
 type loggerOptions struct {
 	logLevel int
-	logFile  os.File
+	logFile  *os.File
+}
+
+func NewLoggerOptions() *loggerOptions {
+	opts := &loggerOptions{
+		logLevel: 0,
+		logFile:  nil,
+	}
+	return opts
+}
+
+func (l *loggerOptions) SetLogLevel(lv int) {
+	switch {
+	case lv < 0:
+		l.logLevel = 0
+	case lv > 3:
+		l.logLevel = 3
+	default:
+		l.logLevel = lv
+	}
 }
