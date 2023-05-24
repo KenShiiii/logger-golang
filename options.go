55890a1a93d38b72eb0be13dd1fcee07d9ae113f
Kenshi Kuo <kenshi_kuo@hitrontech.com>
2023-05-24 14:33:03 +0800

    add comment

--- options.go
+++ options.go
@@ -17,6 +17,17 @@ func NewLoggerOptions() *loggerOptions {
 	return opts
 }
 
+// SetLogLevel sets the log level for the logger.
+// The log level determines which log messages are recorded based on their severity.
+// The input lv should be an integer value representing the desired log level:
+//   - Values less than 0 will set the log level to Debug.
+//   - Values greater than 4 will set the log level to Fatal.
+//   - Values between 0 and 4 (inclusive) will set the log level accordingly.
+//   - 0: Debug
+//   - 1: Info
+//   - 2: Warning
+//   - 3: Error
+//   - 4: Fatal
 func (l *loggerOptions) SetLogLevel(lv int) {
 	switch {
 	case lv < 0:
