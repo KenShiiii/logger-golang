c78129e129e9477bac0da846f95005345eef296a
Kenshi Kuo <kenshi_kuo@hitrontech.com>
2023-05-24 14:21:56 +0800

    add enableStdoutLogging option & define log level as constant

--- options.go
+++ options.go
@@ -3,14 +3,16 @@ package splog
 import "os"
 
 type loggerOptions struct {
-	logLevel int
+	logLevel uint
 	logFile  *os.File
+	stdout   bool
 }
 
 func NewLoggerOptions() *loggerOptions {
 	opts := &loggerOptions{
 		logLevel: 0,
 		logFile:  nil,
+		stdout:   true,
 	}
 	return opts
 }
@@ -18,10 +20,18 @@ func NewLoggerOptions() *loggerOptions {
 func (l *loggerOptions) SetLogLevel(lv int) {
 	switch {
 	case lv < 0:
-		l.logLevel = 0
-	case lv > 3:
-		l.logLevel = 3
+		l.logLevel = LevelDebug
+	case lv > 4:
+		l.logLevel = LevelFatal
 	default:
-		l.logLevel = lv
+		l.logLevel = uint(lv)
 	}
+}
+
+// EnableStdoutLogging enables or disables logging to standard output (stdout).
+// If enable is set to true, log messages will be written to stdout in addition
+// to the log file. If enable is set to false, log messages will only be written
+// to the log file.
+func (l *loggerOptions) EnableStdoutLogging(enable bool) {
+	l.stdout = enable
 }
